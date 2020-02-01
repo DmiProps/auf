7b013af1067d4b257d169788517dbb99bb745126
dmiprops <dmi.develop@gmail.com>
2020-02-01 15:19:18 +0300

    Set disabled signup form

--- www/scripts/handlers.js
+++ www/scripts/handlers.js
@@ -1,14 +1,29 @@
 // Reset class 'fault'
-function resetFault(item) {
+function resetFault(el) {
 
-  if (item.classList.contains('fault')) {
-    item.classList.remove('fault');
+  if (el.classList.contains('fault')) {
+    el.classList.remove('fault');
   }
   
 }
 
 // Validate and send registration data
 function signUp() {
+
+  document.getElementById('userMsg').hidden = true;
+  document.getElementById('emailMsg').hidden = true;
+  document.getElementById('phoneMsg').hidden = true;
+
+  document.getElementById('userInput').disabled = true;
+  document.getElementById('passInput').disabled = true;
+  document.getElementById('emailInput').disabled = true;
+  document.getElementById('phoneInput').disabled = true;
+
+  el = document.getElementById('signupBtn');
+  el.classList.add('busy');
+  if (el.classList.contains('button-primary')) {
+    el.classList.remove('button-primary');
+  }
 
   data = {
     user: document.getElementById('userInput').value,
@@ -36,6 +51,15 @@ function signUp() {
 
   axios.post('/signup', data)
   .then(response => {
+    document.getElementById('userInput').disabled = false;
+    document.getElementById('passInput').disabled = false;
+    document.getElementById('emailInput').disabled = false;
+    document.getElementById('phoneInput').disabled = false;
+    el = document.getElementById('signupBtn');
+    el.classList.add('button-primary');
+    if (el.classList.contains('busy')) {
+      el.classList.remove('busy');
+    }
     if (response.data.Ok == true) {
       window.location = '/www/signin.html';
     } else {
