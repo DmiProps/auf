d2bf73879ba98f272efd5d16c5573c237a2362a3
dmiprops <dmi.develop@gmail.com>
2020-02-13 23:47:35 +0300

    Start sign in

--- www/scripts/handlers.js
+++ www/scripts/handlers.js
@@ -186,3 +186,72 @@ function resendLink() {
   .catch(error => console.log(error));
 
 }
+
+// Validate and send authentification data
+function signIn() {
+
+  // 1. Get sign up data
+  data = {
+    user: document.getElementById('userInput').value,
+    pass: document.getElementById('passInput').value
+  }
+
+  // 2. Validation by client method
+  /*if (!validateSignUpData(data)) {
+    return
+  }
+
+  // 3. Blocking elements
+  document.getElementById('userInput').disabled = true;
+  document.getElementById('passInput').disabled = true;
+  document.getElementById('emailInput').disabled = true;
+  document.getElementById('phoneInput').disabled = true;
+
+  document.getElementById('userMsg').hidden = true;
+  document.getElementById('emailMsg').hidden = true;
+  document.getElementById('phoneMsg').hidden = true;
+
+  el = document.getElementById('signupBtn');
+  el.classList.add('busy');
+  if (el.classList.contains('button-primary')) {
+    el.classList.remove('button-primary');
+  }
+
+  // 4. Send data to server
+  axios.post('/signup', data)
+  .then(response => {
+
+    // 5. Unblocking elements
+    document.getElementById('userInput').disabled = false;
+    document.getElementById('passInput').disabled = false;
+    document.getElementById('emailInput').disabled = false;
+    document.getElementById('phoneInput').disabled = false;
+    el = document.getElementById('signupBtn');
+    el.classList.add('button-primary');
+    if (el.classList.contains('busy')) {
+      el.classList.remove('busy');
+    }
+
+    if (response.data.Ok == true) {
+
+      // 6. Go to activate-link
+      window.location = '/www/activate-link.html';
+
+    } else {
+
+      // 7. Show validation messages
+      el = document.getElementById('userMsg');
+      el.innerText = response.data.UserMsg;
+      el.hidden = (response.data.UserMsg == '');
+      el = document.getElementById('emailMsg');
+      el.innerText = response.data.EmailMsg;
+      el.hidden = (response.data.EmailMsg == '');
+      el = document.getElementById('phoneMsg');
+      el.innerText = response.data.PhoneMsg;
+      el.hidden = (response.data.PhoneMsg == '');
+      
+    }
+  })
+  .catch(error => console.log(error));*/
+  
+}
